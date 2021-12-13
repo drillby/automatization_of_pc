5249e633a0ddde211a24570b5295ff0c26861102
Pavel Podrazký <podrazkypavel002@gmail.com>
2021-12-13 20:27:55 +0100

    wol update

--- modules/wake_on_lan.py
+++ modules/wake_on_lan.py
@@ -1,4 +1,6 @@
-from wakeonlan import send_magic_packet
+from wakeonlan import send_magic_packet, create_magic_packet
+
+create_magic_packet("DC-41-A9-E2-FE-0F")
 
 send_magic_packet("DC-41-A9-E2-FE-0F", ip_address="192.168.132.102",
                   port=80, interface="192.168.132.1")
