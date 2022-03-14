dac9c51e3b7f051548ef620ec768ba107b9cf267
Pavel <podrazkypavel002@gmail.com>
2022-03-14 22:48:30 +0100

    cfg parser

--- web/utils/cfg_parser.py
+++ web/utils/cfg_parser.py
@@ -1,7 +1,17 @@
 import configparser
+from configparser import ConfigParser
 from os import PathLike
 
 
-def return_cfg_file(path: PathLike):
+def return_cfg_file(path: PathLike) -> ConfigParser:
+    """Returns object for parsing configuration file
+
+    Args:
+        path (PathLike): path to the configuration file
+
+    Returns:
+        ConfigParser: ConfigParser object for parsing configuration file
+    """
     config_parser = configparser.ConfigParser()
-    return config_parser.read(path)
+    config_parser.read(path)
+    return config_parser
