37e33d46135c96bdf0d2f38d070ca6262530fce7
Pavel <podrazkypavel002@gmail.com>
2022-06-10 23:07:16 +0200

    deleted unused url paths

--- backend/Spotify/urls.py
+++ backend/Spotify/urls.py
@@ -8,13 +8,6 @@ urlpatterns = [
     path("current_artist", views.current_artist, name="current_artist"),
     path("active_device", views.active_device, name="active_device"),
     path("current_info", views.current_info, name="current_info"),
-
-    path("run_spotify", views.run_spotify, name="run_spotify"),
-    path("change_volume", views.volume, name="change_volume"),
-    path(
-        "add_recomended_songs_to_queue", views.add_to_queue, name="add_songs_to_queue"
-    ),
-    path("live_update", views.live_update, name="live_update"),
     path("set_volume", views.set_volume, name="set_volume"),
     path("add_songs_to_queue", views.add_songs_to_queue, name="add_songs_to_queue"),
 ]
