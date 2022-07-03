73e585fef808bb9eae99433ebed91d7362ce531c
Pavel <podrazkypavel002@gmail.com>
2022-07-03 21:40:29 +0200

    infotainment frontend init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Spotify from "./pages/Spotify";
 import Tests from "./pages/Tests";
+import Infotainment from "./pages/Infotainment";
 function App() {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/spotify" element={<Spotify />}></Route>
 				<Route path="/dev" element={<Tests />}></Route>
+				<Route path="/infotainment" element={<Infotainment />}></Route>
 			</Routes>
 		</Router>
 	);
---
+++ frontend/src/pages/Infotainment.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Infotainment() {
+	return <div>Infotainment</div>;
+}
+
+export default Infotainment;
